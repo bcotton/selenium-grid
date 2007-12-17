5df81528e3180567f6c8505d603cbba2b9ed13cd
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2007-12-17 20:41:08 +0000

    Created HubServer and HubServlet JRuby tests.
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@60 22590ab4-ef3b-0410-9bef-a9f5a1089f47

---
+++ hub/src/test/unit/com/thoughtworks/selenium/grid/hub/hub_server_test.rb
@@ -0,0 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + "/../../../../../unit_test_helper")
+
+include_class 'com.thoughtworks.selenium.grid.hub.HubServer'
+
+class HubServerTest < Test::Unit::TestCase
+
+  xtest "tests not implemented", "must do something" do
+    assert_true true
+  end
+
+end
