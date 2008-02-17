0ddd5fd7be9d699e52cb0e5f91241c6f90c16989
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-17 22:59:26 +0000

    Changed Ec2 integration approach
    git-svn-id: https://svn.openqa.org/svn/selenium-grid/trunk@169 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/ec2/lib/selenium_grid/aws/ec2.rb
+++ examples/ec2/lib/selenium_grid/aws/ec2.rb
@@ -1,7 +1,5 @@
 require 'rubygems'
 require "yaml"
-require 'ec2'
-require 'forwardable'
 require File.dirname(__FILE__) + '/cloud'
 require File.dirname(__FILE__) + '/ec2_client'
 require File.dirname(__FILE__) + '/server'
--- examples/ec2/lib/selenium_grid/aws/server.rb
+++ examples/ec2/lib/selenium_grid/aws/server.rb
@@ -10,41 +10,37 @@ module SeleniumGrid
         self.instance_id = instance_id
       end       
 
-      def self.boot(image_id)
-        new(run_instance(:image_id => image_id))
+      def self.boot(ami, options = {})
+        new launch(ami, options)
       end
 
-      def self.boot_and_acquire_dns(image_id)
-        server = boot image_id
+      def self.boot_and_acquire_dns(ami, options)
+        server = boot(ami, options)
+        server.wait_for_dns
+      end
+
+      def wait_for_dns
         puts "Fetching DNS Info..."
-        until server.dns_allocated?
+        until dns_allocated?
           sleep 2
           putc "."
-          server.refresh_instance_info
+          refresh_status
         end
         puts
+        self
       end
-
+      
       def dns_allocated?
         public_dns != nil && public_dns != "" &&
         private_dns != nil && private_dns != ""
       end
       
-      def refresh_instance_info
-        instance_info = fetch_instance_info
-        @public_dns = instance_info[:public_dns]
-        @private_dns = instance_info[:private_dns]
+      def refresh_status
+        info = self.class.describe instance_id
+        public_dns = info[:public_dns]
+        private_dns = info[:private_dns]
       end
-      
-      def fetch_instance_info
-        response = self.class.driver.describe_instances :instance_id => instance_id
-        instance_info = response.reservationSet.item.first.instancesSet.item.first
-        { :public_dns => instance_info.dnsName, 
-          :private_dns => instance_info.privateDnsName,
-          :status => instance_info.instanceState.name
-        }
-      end
-            
+                  
     end
 
   end
