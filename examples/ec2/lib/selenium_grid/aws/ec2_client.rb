7ec0acb1f6800367e1d9a698f6870cedb5c4cbe1
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-17 22:59:26 +0000

    Changed Ec2 integration approach
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@169 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/ec2/lib/selenium_grid/aws/ec2_client.rb
+++ examples/ec2/lib/selenium_grid/aws/ec2_client.rb
@@ -1,37 +1,44 @@
 module SeleniumGrid
   module AWS
     module Ec2Client
-      extend Forwardable
       
-      DEFAULT_AUTHORIZE_OPTIONS = {:ip_protocol => "tcp", :group_name => "default" }
-
-      # def_delegator :driver, :describe_instances, :describe_instances
-
-      def describe_instances(options)
-        driver.describe_instances options
+      def describe(ami)
+        output = ec2_shell "ec2-describe-instances #{ami}"
+        output =~ /INSTANCE\s+(i-.*)$/
+        fields = $1.split(/\s+/)
+        if output =~ /running/
+          {:instance_id => fields[0],
+           :ami => fields[1],
+           :public_dns => fields[2],
+           :private_dns => fields[3],
+           :status => fields[4] }
+        else 
+          {:instance_id => fields[0],
+           :ami => fields[1],
+           :status => fields[2] }
+        end
       end
             
-      def run_instance(options)
-        response = driver.run_instances(:image_id => options[:image_id])
-        response.instancesSet.item.first.instanceId        
+      def launch(ami, options ={})
+        output = ec2_shell "ec2-run-instances #{ami} -k #{options[:keypair]}"
+        output =~ /INSTANCE\s+(i-\S+)\s+ami-/
+        $1
       end
             
-      def authorize(options)
-        actual_options = DEFAULT_AUTHORIZE_OPTIONS.merge(options)
-        actual_options[:from_port] = actual_options[:to_port] = options[:port]
-        puts "Opening port #{options[:port]} for #{actual_options[:group_name]} group..."        
-        driver.authorize_security_group_ingress(actual_options)
+      def authorize_port(port)
+        puts "Opening port #{port}..."        
+        ec2_shell "ec2-authorize default -p #{port}"
       end
-      
-      def driver
-        return @ec2_driver unless @ec2_driver.nil?
-        
-        access_key_id = ENV["AMAZON_ACCESS_KEY_ID"]
-        secret_access_key = ENV["AMAZON_SECRET_ACCESS_KEY"]
-        if access_key_id.nil? 
-          access_key_id, secret_access_key = IO.read("/Volumes/Private Disk/New Stuff Mac Only/Amazon EC2/keys.txt").chop.split(/ /)
-        end        
-        @ec2_driver = EC2::Base.new(:access_key_id => access_key_id, :secret_access_key => secret_access_key)
+            
+      def ec2_shell(command)
+        puts 
+        puts command
+        output = `${EC2_HOME}/bin/#{command}`
+        # raise "Error running command '#{command}" unless 0 == $?
+        puts ">>>> got"
+        puts output
+        puts ">>>>>"
+        output
       end
       
     end
