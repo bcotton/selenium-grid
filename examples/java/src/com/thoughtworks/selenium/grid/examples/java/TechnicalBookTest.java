fa4bc19d3a35ade20d29933f2768a4bdd20675d1
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-18 20:56:58 +0000

    Java Example: Cleaning up Idea project.
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@182 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/java/src/com/thoughtworks/selenium/grid/examples/java/TechnicalBookTest.java
+++ examples/java/src/com/thoughtworks/selenium/grid/examples/java/TechnicalBookTest.java
@@ -10,25 +10,13 @@ import static org.testng.AssertJUnit.assertEquals;
 
 
 /**
- * Traditional Selenium Test checking the quality of Amazon comments ;o).
- * <br/>
- * Each test request a different browser/environment.
- * <br/>
- * <code>testAmazonOnFirefox</code> can run against a plain vanilla
- * Selenium remote control.
- * <br/>
- * The other tests need to run again a Selenium Hub: They demonstrate
- * the capacity of requesting a specific environment per
- * test/test suite/build. Of course these environments must be defined
- * on the Hub and at least one remote control must register as providing
- * this particular environment.
  */
 public class TechnicalBookTest {
 
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void firstTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+    public void refactoringBookTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
         startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
         session().open("/");
         session().type("twotabsearchtextbox", "refactoring");
@@ -45,21 +33,63 @@ public class TechnicalBookTest {
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (2).")
     @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void secondTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+    public void gofBookTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
         startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
         session().open("/");
-        session().type("twotabsearchtextbox", "refactoring");
+        session().type("twotabsearchtextbox", "design patterns");
         session().click("navGoButtonPanel");
         session().waitForPageToLoad("60000");
         assertTrue(session().getLocation().startsWith("http://www.amazon.com/s/ref="));
-        session().click("//img[@alt='Refactoring: Improving the Design of Existing Code (The Addison-Wesley Object Technology Series)']");
-        session().waitForPageToLoad("60000");
-        assertEquals("1", session().getValue("name=quantity"));
-        assertTrue(session().isTextPresent("excellent"));
-        assertTrue(session().isTextPresent("Hidden Treasure"));
         closeSeleniumSession();
     }
 
+    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (2).")
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    public void operatingSystems(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().open("/");
+        session().type("twotabsearchtextbox", "operating system");
+        session().click("navGoButtonPanel");
+        session().waitForPageToLoad("60000");
+        assertTrue(session().getLocation().startsWith("http://www.amazon.com/s/ref="));
+        closeSeleniumSession();
+    }
+
+    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (2).")
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    public void pragProgtest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().open("/");
+        session().type("twotabsearchtextbox", "pragmatic programmer");
+        session().click("navGoButtonPanel");
+        session().waitForPageToLoad("60000");
+        assertTrue(session().getLocation().startsWith("http://www.amazon.com/s/ref="));
+        closeSeleniumSession();
+    }
+
+    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (2).")
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    public void extremeProgrammingTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().open("/");
+        session().type("twotabsearchtextbox", "pragmatic programmer");
+        session().click("navGoButtonPanel");
+        session().waitForPageToLoad("60000");
+        assertTrue(session().getLocation().startsWith("http://www.amazon.com/s/ref="));
+        closeSeleniumSession();
+    }
+
+    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (2).")
+    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
+    public void analysisPatternsTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
+        startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
+        session().open("/");
+        session().type("twotabsearchtextbox", "Ananlysis Patterns");
+        session().click("navGoButtonPanel");
+        session().waitForPageToLoad("60000");
+        assertTrue(session().getLocation().startsWith("http://www.amazon.com/s/ref="));
+        closeSeleniumSession();
+    }
 
 
 }
