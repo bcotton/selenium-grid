f35ef9916cf72b73c83185e1a7833146da916b98
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-23 05:09:34 +0000

    Java Example: More reasonable thread count.
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@198 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/java/src/com/thoughtworks/selenium/grid/examples/java/BookTest.java
+++ examples/java/src/com/thoughtworks/selenium/grid/examples/java/BookTest.java
@@ -9,25 +9,27 @@ import static org.testng.AssertJUnit.assertTrue;
  */
 public class BookTest {
 
+    public static final String TIMEOUT = "120000";
+
     protected void checkBook(String title, String thumbnail, String keywords, String isbn) {
         session().open("/");
         session().select("url", "Books");
         session().type("twotabsearchtextbox", keywords);
         session().click("navGoButtonPanel");
-        session().waitForPageToLoad("60000");
+        session().waitForPageToLoad(TIMEOUT);
         assertTrue(session().isTextPresent(title));
         session().click("//img[@alt=\"" + thumbnail + "\"]");
-        session().waitForPageToLoad("60000");
+        session().waitForPageToLoad(TIMEOUT);
         assertEquals("1", session().getValue("name=quantity"));
         assertTrue(session().isTextPresent("ISBN-10: " + isbn));
         session().click("link=Explore similar items");
-        session().waitForPageToLoad("60000");
+        session().waitForPageToLoad(TIMEOUT);
         session().goBack();
-        session().waitForPageToLoad("60000");
+        session().waitForPageToLoad(TIMEOUT);
         assertEquals("1", session().getValue("quantity"));
         session().select("quantity", "label=5");
         session().click("submit.add-to-cart");
-        session().waitForPageToLoad("60000");
+        session().waitForPageToLoad(TIMEOUT);
         assertTrue(session().isTextPresent("Added to your\nShopping Cart:"));
         assertTrue(session().isTextPresent(title));
         assertTrue(session().isTextPresent("quantity: 5"));
