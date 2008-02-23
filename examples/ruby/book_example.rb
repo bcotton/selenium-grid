4f0c01773bf0b6d855dc007398f19ea24dff7e2a
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-23 06:03:06 +0000

    Ruby Example: Removing Spec UI dependency
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@201 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/ruby/book_example.rb
+++ examples/ruby/book_example.rb
@@ -5,9 +5,8 @@ module BookExample
     @selenium.select "url", "Books"
     @selenium.type "twotabsearchtextbox", options[:keywords]
     @selenium.click_and_wait "navGoButtonPanel"
-    # @selenium.is_text_present(options[:title]).should be_true
-    @selenium.click "//img[@alt=\"#{options[:anchor]}\"]"
-    @selenium.wait_for_page_to_load(60000);
+    @selenium.is_text_present(options[:title]).should be_true
+    @selenium.click_and_wait "//img[@alt=\"#{options[:anchor]}\"]"
     @selenium.field("name=quantity").should == "1"
     @selenium.is_text_present("ISBN-10: #{options[:isbn]}").should be_true
     @selenium.click_and_wait "link=Explore similar items"
