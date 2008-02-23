83b23a27aeaeab1d60ae1ecb7b48261984fb90d5
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-23 07:29:13 +0000

    Ruby Example: More Tests
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@203 22590ab4-ef3b-0410-9bef-a9f5a1089f47

---
+++ examples/ruby/rubyist_spec.rb
@@ -0,0 +1,23 @@
+require File.expand_path(File.dirname(__FILE__) + "/lib/view_test_helper")
+
+describe "Good Rubyist" do
+  include BookExample
+  
+  it "embraces Ruby" do
+    as_described_on_amazon \
+      :title => "Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
+      :anchor => "Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
+      :keywords => "Ruby",
+      :isbn => "0974514055"
+      
+  end
+  
+  it "has style" do
+    as_described_on_amazon \
+      :title => "The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming",
+      :anchor => "The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming (2nd Edition) (Addison-Wesley Professional Ruby Series)",
+      :keywords => "Ruby",
+      :isbn => "0672328844"
+  end
+  
+end
