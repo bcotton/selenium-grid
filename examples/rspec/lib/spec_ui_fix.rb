54fb0e24ba5204fa775285e9eeb38779889ab84c
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-02-23 07:31:30 +0000

    Ruby Examples: Renamed rspec to ruby
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@204 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/rspec/lib/spec_ui_fix.rb
+++
@@ -1,7 +0,0 @@
-# Workaround uninitialized constant Spec::Runner::Formatter
-require 'rubygems'
-require 'spec'
-require 'spec/runner/formatter/html_formatter'
-require 'spec/ui' 
-require File.dirname(__FILE__) + '/spec/screenshot_saver'
-require File.dirname(__FILE__) + '/spec/screenshot_formatter'
