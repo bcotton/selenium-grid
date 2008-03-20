89451ee0b8b774115c51c60852b04ff61dea0065
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-03-20 06:39:50 +0000

    Java Example: Use @BeforeTest @AfterTest to start/stop session
    git-svn-id: https://svn.openqa.org/svn/selenium-grid/trunk@242 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/java/src/com/thoughtworks/selenium/grid/examples/java/RubyBooksTest.java
+++ examples/java/src/com/thoughtworks/selenium/grid/examples/java/RubyBooksTest.java
@@ -1,8 +1,5 @@
 package com.thoughtworks.selenium.grid.examples.java;
 
-import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.closeSeleniumSession;
-import static com.thoughtworks.selenium.grid.tools.ThreadSafeSeleniumSessionStorage.startSeleniumSession;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
@@ -11,73 +8,43 @@ import org.testng.annotations.Test;
 public class RubyBooksTest extends BookTest {
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
-    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void programmingRubyBookTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
-        try {
-            startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
-            checkBook("Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
-                    "Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
-                    "Ruby",
-                    "0974514055");
-        } finally {
-            closeSeleniumSession();
-        }
+    public void programmingRubyBookTest() throws Throwable {
+        checkBook("Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
+                "Programming Ruby: The Pragmatic Programmers' Guide, Second Edition",
+                "Ruby",
+                "0974514055");
     }
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
-    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void rubyWayBookTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
-        try {
-            startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
-            checkBook("The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming",
-                    "The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming (2nd Edition) (Addison-Wesley Professional Ruby Series)",
-                    "Ruby",
-                    "0672328844");
-        } finally {
-            closeSeleniumSession();
-        }
+    public void rubyWayBookTest() throws Throwable {
+        checkBook("The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming",
+                "The Ruby Way, Second Edition: Solutions and Techniques in Ruby Programming (2nd Edition) (Addison-Wesley Professional Ruby Series)",
+                "Ruby",
+                "0672328844");
     }
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
-    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void agileWebDevelomentWithRailsTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
-        try {
-            startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
-            checkBook("Agile Web Development with Rails",
-                    "Agile Web Development with Rails, 2nd Edition",
-                    "Ruby",
-                    "0977616630");
-        } finally {
-            closeSeleniumSession();
-        }
+    public void agileWebDevelomentWithRailsTest() throws Throwable {
+        checkBook("Agile Web Development with Rails",
+                "Agile Web Development with Rails, 2nd Edition",
+                "Ruby",
+                "0977616630");
     }
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
-    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void advancedRailsRecipesTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
-        try {
-            startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
-            checkBook("Advanced Rails Recipes: 72 New Ways to Build Stunning Rails Apps",
-                    "Advanced Rails Recipes: 72 New Ways to Build Stunning Rails Apps",
-                    "Rails Recipes",
-                    "0978739221");
-        } finally {
-            closeSeleniumSession();
-        }
+    public void advancedRailsRecipesTest() throws Throwable {
+        checkBook("Advanced Rails Recipes: 84 New Ways to Build Stunning Rails Apps",
+                "Advanced Rails Recipes: 84 New Ways to Build Stunning Rails Apps",
+                "Rails Recipes",
+                "0978739221");
     }
 
     @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
-    @Parameters({"seleniumHost", "seleniumPort", "browser", "webSite"})
-    public void railsWayTest(String seleniumHost, int seleniumPort, String browser, String webSite) throws Throwable {
-        try {
-            startSeleniumSession(seleniumHost, seleniumPort, browser, webSite);
-            checkBook("The Rails Way",
-                    "The Rails Way (Addison-Wesley Professional Ruby Series)",
-                    "Rails",
-                    "0321445619");
-        } finally {
-            closeSeleniumSession();
-        }
+    public void railsWayTest() throws Throwable {
+        checkBook("The Rails Way",
+                "The Rails Way (Addison-Wesley Professional Ruby Series)",
+                "Rails",
+                "0321445619");
     }
 
 }
