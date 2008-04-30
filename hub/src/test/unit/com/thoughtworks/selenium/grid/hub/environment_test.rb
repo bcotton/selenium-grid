5d12762fed7c12cc2064caa353871aa9a8f6e759
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-04-30 16:40:37 +0000

    Hub & Doc: Converting back JRuby test to Java. Merge Shannon's doc and existing EC2 documentation
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@273 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- hub/src/test/unit/com/thoughtworks/selenium/grid/hub/environment_test.rb
+++ hub/src/test/unit/com/thoughtworks/selenium/grid/hub/environment_test.rb
@@ -6,53 +6,11 @@ include_class 'com.thoughtworks.selenium.grid.hub.remotecontrol.DynamicRemoteCon
 
 class EnvironmentTest < Test::Unit::TestCase
 
-  test "Throws an IllegalArgumentException when name is null" do
-    assert_throws("java.lang.IllegalArgumentException", "name cannot be null") do
-      a_pool = Class.new { include DynamicRemoteControlPool }.new
-      Environment.new(nil, "a browser", a_pool)  
-    end
-  end
-  
-  test "Name is the name provided to constructor" do
-    assert_equal "an environment", Environment.new("an environment", "a browser", nil).name
-  end
-
-  test "Browser is the browser provided to constructor" do
-    assert_equal "some browser", Environment.new("an environment", "some browser", nil).browser
-  end
-
-  test "Pool is the pool provided to constructor" do
-    a_pool = Class.new { include DynamicRemoteControlPool }.new
-    assert_equal a_pool, Environment.new("an environment", "some browser", a_pool).pool
-  end
-
-  test "An environment is not equal to a random object" do
-    assert_not_equal Environment.new("an env", "a browser", nil), "random"
-  end
-
-  test "2 environments with the same name are equals" do
-    assert_equal Environment.new("an environment", "*firefox", nil), Environment.new("an environment", "*ie", nil)  
-  end
-
-  test "2 environments with different names are not equals" do
-    assert_not_equal Environment.new("an environment", "*browser", nil),
-                     Environment.new("another environment", "*browser", nil)
-
-  end
-
-  test "2 environments with the same name have the same hashcode" do
-    assert_equal Environment.new("an environment", "*browser", nil).hash_code,
-                 Environment.new("an environment", "*browser", nil).hash_code  
-  end
-
-  test "2 environments with different names do not have the same hashcode" do
-    assert_not_equal Environment.new("an environment", "*browser", nil).hash_code,
-                     Environment.new("another environment", "*browser", nil).hash_code
-  end
+  # Only here for reference until I introduce a JRuby servlet
 
   test "to_string returns a human friendly string" do
     assert_equal "[Environment name='Firefox / Linux', browser='*chrome']",
-                 Environment.new("Firefox / Linux", "*chrome", nil).to_string() 
+                 Environment.new("Firefox / Linux", "*chrome", nil).to_string()
   end
 
 end
--- hub/src/test/unit/java/com/thoughtworks/selenium/grid/hub/ApplicationRegistryTest.java
+++ hub/src/test/unit/java/com/thoughtworks/selenium/grid/hub/ApplicationRegistryTest.java
@@ -1,37 +1,35 @@
 package com.thoughtworks.selenium.grid.hub;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
 import org.junit.Test;
 
 
 public class ApplicationRegistryTest {
 
     @Test
-    public void somethingHappens() {
-        assertEquals(true, true);
+    public void registryReturnsAValidInstance() {
+        assertFalse(null == ApplicationRegistry.registry());
     }
 
+    public void registryReturnsASingletonInstance() {
+        assertSame(ApplicationRegistry.registry(), ApplicationRegistry.registry());
+    }
 
-//    @Test
-//    public void registryReturnsAValidInstance() {
-//        assertFalse(null == ApplicationRegistry.registry());
-//    }
-//
-//    @Test
-//    public void remoteControlPoolReturnsAValidPool() {
-//        assertFalse(null == ApplicationRegistry.registry().remoteControlPool());
-//    }
-//
-//    @Test
-//    public void environmentManagerReturnsAUniqueInstance() {
-//        assertSame(ApplicationRegistry.registry().environmentManager(),
-//                   ApplicationRegistry.registry().environmentManager());
-//    }
+    @Test
+    public void remoteControlPoolReturnsAValidPool() {
+        assertFalse(null == ApplicationRegistry.registry().remoteControlPool());
+    }
 
-//    @Test
-//    public void gridConfigurationReturnsAUniqueInstance() {
-//        assertSame(ApplicationRegistry.registry().gridConfiguration(),
-//                   ApplicationRegistry.registry().gridConfiguration());
-//    }
+    @Test
+    public void environmentManagerReturnsAUniqueInstance() {
+        assertSame(ApplicationRegistry.registry().environmentManager(),
+                   ApplicationRegistry.registry().environmentManager());
+    }
+
+    @Test
+    public void gridConfigurationReturnsAUniqueInstance() {
+        assertSame(ApplicationRegistry.registry().gridConfiguration(),
+                   ApplicationRegistry.registry().gridConfiguration());
+    }
 
 }
