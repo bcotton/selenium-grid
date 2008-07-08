c200aa9d8ed0a10268039e2c554e0e147453d306
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-07-08 01:19:14 +0000

    Ruby Example: Use new generation screenshot formatter
    git-svn-id: http://svn.openqa.org/svn/selenium-grid/trunk@302 22590ab4-ef3b-0410-9bef-a9f5a1089f47

--- examples/ruby/rubyist_spec.rb
+++ examples/ruby/rubyist_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + "/lib/view_test_helper")
+require File.expand_path(File.dirname(__FILE__) + "/lib/spec_helper")
 
 describe "Good Rubyist" do
   include BookExample
---
+++ examples/ruby/vendor/selenium-client-1.1/lib/selenium/client/base.rb
@@ -0,0 +1,52 @@
+# Copyright 2006 ThoughtWorks, Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#
+#
+# Original code by Aslak Hellesoy and Darren Hobbs
+#
+module Selenium
+  module Client
+    
+    module Base
+      include Selenium::Client::SeleneseClient
+      include Selenium::Client::GeneratedDriver
+  
+      def initialize(server_host, server_port, browserStartCommand, browserURL, timeout=30000)
+        @server_host = server_host
+        @server_port = server_port
+        @browserStartCommand = browserStartCommand
+        @browserURL = browserURL
+        @timeout = timeout
+        @extension_js = ""
+      end
+      
+      def set_extension_js(extension_js)
+        @extension_js = extension_js
+      end
+      
+      def start()
+        result = get_string("getNewBrowserSession", [@browserStartCommand, @browserURL, @extension_js])
+        @session_id = result
+      end
+      
+      def stop()
+        do_command("testComplete", [])
+        @session_id = nil
+      end
+
+    end
+  
+  end
+end
