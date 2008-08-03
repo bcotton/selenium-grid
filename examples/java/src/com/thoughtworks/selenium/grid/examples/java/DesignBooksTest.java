7fa36b88839e5c0f4794a0529e2c1de80b62749a
Philippe Hanrigou <philippe.hanrigou@gmail.com>
2008-08-02 18:07:19 -0700

    Java Example: Fixed a bug in Java example instrumentation: TestNG was not running the @AfterTest on test failures, leaking browser sessions.

--- examples/java/src/com/thoughtworks/selenium/grid/examples/java/DesignBooksTest.java
+++ examples/java/src/com/thoughtworks/selenium/grid/examples/java/DesignBooksTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
  */
 public class DesignBooksTest extends BookTest {
 
-    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
+    @Test(groups = {"example", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     public void refactoring() throws Throwable {
         checkBook("Refactoring: Improving the Design of Existing Code",
                 "Refactoring: Improving the Design of Existing Code (The Addison-Wesley Object Technology Series)",
@@ -16,7 +16,7 @@ public class DesignBooksTest extends BookTest {
     }
 
 
-    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
+    @Test(groups = {"example", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     public void designPatterns() throws Throwable {
         checkBook("Design Patterns: Elements of Reusable Object-Oriented Software",
                 "Design Patterns: Elements of Reusable Object-Oriented Software (Addison-Wesley Professional Computing Series)",
@@ -24,7 +24,7 @@ public class DesignBooksTest extends BookTest {
                 "0201633612");
     }
 
-    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
+    @Test(groups = {"example", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     public void testDrivenDevelopment() throws Throwable {
         checkBook("Test Driven Development: By Example",
                 "Test Driven Development: By Example (The Addison-Wesley Signature Series)",
@@ -32,7 +32,7 @@ public class DesignBooksTest extends BookTest {
                 "0321146530");
     }
 
-    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
+    @Test(groups = {"example", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     public void enterpriseDesignPatterns() throws Throwable {
         checkBook("Patterns of Enterprise Application Architecture",
                 "Patterns of Enterprise Application Architecture (The Addison-Wesley Signature Series)",
@@ -40,7 +40,7 @@ public class DesignBooksTest extends BookTest {
                 "0321127420");
     }
 
-    @Test(groups = {"demo", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
+    @Test(groups = {"example", "firefox", "default"}, description = "Test Amazon Comment Quality Using the Firefox Web Browser (1).")
     public void domainDrivenDesign() throws Throwable {
         checkBook("Domain-Driven Design: Tackling Complexity in the Heart of Software",
                 "Domain-Driven Design: Tackling Complexity in the Heart of Software",
