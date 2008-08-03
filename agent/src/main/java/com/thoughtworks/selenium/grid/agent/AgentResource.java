a4e885e0365965797267a677e8dfcdffac8e379e
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-08-03 17:04:45 +0000

    Adding missing files
    git-svn-id: https://svn.openqa.org/svn/selenium-grid/trunk@346 22590ab4-ef3b-0410-9bef-a9f5a1089f47

---
+++ agent/src/main/java/com/thoughtworks/selenium/grid/agent/AgentResource.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.selenium.grid.agent;
+
+import com.thoughtworks.selenium.grid.webserver.Resource;
+
+/**
+ */
+public class AgentResource extends Resource {
+
+    public String process() {
+        return "Selenium Grid Agent";
+    }
+
+}
---
+++ infrastructure/webserver/src/main/java/com/thoughtworks/selenium/grid/webserver/Resource.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.selenium.grid.webserver;
+
+/**
+ */
+public abstract class Resource {
+
+    public abstract String process();
+    
+}
