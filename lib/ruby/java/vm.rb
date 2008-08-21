9d05140058657ecca7942b0bdfc62d570bca11d2
ph7 <ph7@22590ab4-ef3b-0410-9bef-a9f5a1089f47>
2008-08-21 06:27:35 +0000

    Rakefile: Less brutal shutdown tasks for Hub and RCs
    git-svn-id: https://svn.openqa.org/svn/selenium-grid/trunk@362 22590ab4-ef3b-0410-9bef-a9f5a1089f47

---
+++ lib/ruby/java/vm.rb
@@ -0,0 +1,31 @@
+module Java
+
+  class VM
+
+    def run(classname, options)
+      command = [ "java" ]
+      command << "-cp \"#{options[:classpath]}\""
+      command << classname
+      command << jvm_properties(options[:properties])
+      command << options[:args].join(' ') if options[:args]
+      command << ">\"#{options[:log_file]}\" 2>&1" if options[:log_file]
+
+      if options[:background ]
+        if PLATFORM['win32']
+          command.unshift("start")
+        else
+          command << "&"
+        end
+      end
+
+      sh command.join(' ')
+    end
+
+    def jvm_properties(property_hash)
+      return "" unless property_hash
+      property_hash.inject([]) {|memo, (name, value)| memo << "-D#{name}=#{value}" }.join(' ')
+    end
+
+  end
+
+end
